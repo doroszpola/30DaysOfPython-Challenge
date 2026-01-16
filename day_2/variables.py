b79ec26fd4c98cd2a0e58ca15fa1c358c41d5e82
Pola Dorosz <doroszpola@gmail.com>
2026-01-16 17:57:24 +0100

    Dodanie folderu day_2

---
+++ day_2/variables.py
@@ -0,0 +1,59 @@
+## Day2: 30 Days of python programming
+first_name = "Pola"
+last_name = "Dorosz"
+
+full_name = "Pola Dorosz"
+
+country = "Poland"
+
+city = "Łódź"
+
+age = 21
+
+year = 2025
+
+is_married = False
+
+is_true = True
+
+is_light_on = False
+
+a,b,c = 'a', 2, "see"
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(len(first_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+
+diff = num_one - num_two
+
+product = num_one * num_two
+
+division = num_one / num_two
+
+remaiander = num_two % num_one
+
+exp = num_one ** num_two
+
+floor_division = num_one//num_two
+
+circle_radius = 30
+pi = 3.14
+
+area_of_circle = (circle_radius**2) * pi
+circum_of_circle = 2 * circle_radius * pi
+
+first_name_user = input("What's your name? : ")
+print(first_name_user)
+
+help('keywords')
+
+## Done! I am so happy!! :D
+
