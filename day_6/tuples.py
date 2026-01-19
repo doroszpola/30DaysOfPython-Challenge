033c4ced2ea6261316e8888fa77e6189d6c70131
Pola Dorosz <doroszpola@gmail.com>
2026-01-19 17:49:18 +0100

    Dodanie folderu day_6/

---
+++ day_6/tuples.py
@@ -0,0 +1,9 @@
+fruits = ('banana', 'orange', 'mango', 'lemon')
+fruits = list(fruits)
+fruits[0] = 'apple'
+print(fruits)     # ['apple', 'orange', 'mango', 'lemon']
+fruits = tuple(fruits)
+print(fruits)     # ('apple', 'orange', 'mango', 'lemon')
+
+# changing a tuple to a list allows for it to be modified
+
